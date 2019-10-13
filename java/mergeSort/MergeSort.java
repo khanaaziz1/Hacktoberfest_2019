74d2fb5aa4638246d63b559c1b6b9480920a8229
Hesam Ghorbani <50715897+HesamGm@users.noreply.github.com>
2019-10-13 22:15:08 +0330

    Update MergeSort.java
    add some comments to help beginners

--- java/mergeSort/MergeSort.java
+++ java/mergeSort/MergeSort.java
@@ -1,19 +1,21 @@
-package Java.mergeSort;
+package java.mergeSort;
 
 public class Main {
-
+//merge sort works by dividing the array to a single element which is sorted and then sorting while we're merging them on every step to
+//larger elements and we should use a recursive method to divide 
+    
     public static void main(String[] args) {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
         mergeSort(intArray, 0, intArray.length);
 
-        //show the result
+        //iteration to show the result
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
     }
-
-    // { 20, 35, -15, 7, 55, 1, -22 }
+    
+   //recursive method
     public static void mergeSort(int[] input, int start, int end) {
 
         if (end - start < 2) {
@@ -21,12 +23,14 @@ public class Main {
         }
 
         int mid = (start + end) / 2;
+      //divide left side
         mergeSort(input, start, mid);
+      //and right side  
         mergeSort(input, mid, end);
+      //and then merge them on each step  
         merge(input, start, mid, end);
     }
-
-    // { 20, 35, -15, 7, 55, 1, -22 }
+    
     public static void merge(int[] input, int start, int mid, int end) {
 
         if (input[mid - 1] <= input[mid]) {
@@ -37,7 +41,9 @@ public class Main {
         int j = mid;
         int tempIndex = 0;
 
+        // a temporary method to sort
         int[] temp = new int[end - start];
+        
         while (i < mid && j < end) {
             temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
         }
